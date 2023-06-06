472801bfbb2e0162849775fc2efeb936e73d4614
gyduddl <gyduddl4889@naver.com>
2023-06-07 01:24:16 +0900

    기본틀완성

--- src/page/page1.js
+++ src/page/page1.js
@@ -1,1 +1,20 @@
-console.log('DDH hello 브랜치 새로 생성')
+import { useNavigate } from "react-router-dom"
+
+const Page1=()=>{
+    const navigate=useNavigate()
+
+    const goPage2=()=>{
+        navigate('/page2')
+    }
+
+
+    return(
+        <>
+            <div>page1</div>
+            <button onClick={goPage2} className="Start">Get Start</button>
+        </>
+        
+    )
+}
+
+export default Page1
