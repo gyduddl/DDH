91f0630036b284942c0a5882104eba9f01fa9248
kim <hyoyoung_kim@apple.com>
2025-02-04 14:38:17 +0900

    add_date

--- src/page/page1.js
+++ src/page/page1.js
@@ -1,1 +1,20 @@
-console.log('DDH hello')
+import React from 'react';
+import json from '../data/test.json';
+
+const Page1 =()=>{
+    console.log(json)
+
+    
+
+return (
+    <div>
+        <input placeholder='write firstName'/>
+        <input placeholder='write lastName'/>
+        <input placeholder='write phoneNumber'/>
+        <input placeholder='write emailAddress'/>
+        <button>제출하기</button>
+    </div>
+)
+}
+
+export default Page1;
